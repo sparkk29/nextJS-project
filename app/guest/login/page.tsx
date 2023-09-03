b941d6d807dfdbf42219c7265e34283780f81a1a
Sreejit <sreejitsundaran@gmail.com>
2023-09-03 19:24:53 +0530

    new default configs

---
+++ app/guest/login/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const LoginPage: NextPage = () => {
+    return (
+        <div className="">
+            <h1 className="text-4xl  ">Login</h1>
+        </div>
+    );
+}
+
+export default LoginPage;
---
+++ app/guest/register/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const RegisterPage: NextPage = () => {
+  return (
+    <div className="">
+      <h1 className="text-4xl  ">Register</h1>
+    </div>
+  );
+};
+
+export default RegisterPage;
